d94e6e1213fa6bb903d2e229d4a6272ea4717b82
nova-shaw <197215180+nova-shaw@users.noreply.github.com>
2025-07-25 21:16:36 +0900

    Renamed display to media; Source Dock in OBS confirmed able to track mouse pointer movements.

---
+++ controlpanel.js
@@ -0,0 +1,9 @@
+const obsScenes = [
+  'obs-scene-camera1',
+  'obs-scene-camera1-pip-media',
+  'obs-scene-media',
+  'obs-scene-media-pip-camera1',
+  'obs-scene-camera2',
+  'obs-scene-camera1-keyed',
+  'obs-scene-camera1-keyed-pip-media'
+];
