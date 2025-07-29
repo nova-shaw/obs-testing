212bc7f217878461c00d653a0a702e1c3cc2fe06
nova-shaw <197215180+nova-shaw@users.noreply.github.com>
2025-07-29 21:10:08 +0900

    End of work...

--- controlpanel.js
+++ controlpanel.js
@@ -6,6 +6,11 @@ const msgOutput = document.querySelector('#msgOutput');
 obsBC.addEventListener('message', e => {
   msgOutput.textContent = e.data;
   log(e.data);
+  if (e.data.key === 'scene') {
+    obsWS.call('SetCurrentProgramScene', {
+      sceneName: e.data.val
+    });
+  }
 });
 
 const sceneButtonContainer = document.querySelector('#obs-scene-buttons');
@@ -15,10 +20,10 @@ const sceneButtonContainer = document.querySelector('#obs-scene-buttons');
 /**
  * OBS Websockets
  * -> using obs-websocket.js library (imported in HTML on page load)
- * 
+ *
  * The only docs I've been able to find are here:
  * https://github.com/obsproject/obs-websocket/blob/master/docs/generated/protocol.md
- * 
+ *
  * */
 
 const obsWS = new OBSWebSocket();
@@ -35,7 +40,10 @@ const connection = await obsWS.connect('ws://127.0.0.1:4455', 'huwspZ8H7vTp0CYL'
       obsWS.call('GetVideoSettings').then(data => {
         obsSettings = data;
         log(obsSettings);
-      })
+      });
+      obsWS.call('GetHotkeyList').then(data => {
+        log('GetHotkeyList', data);
+      });
     },
     (error) => {
       console.error('OBS not running?', error)
@@ -56,9 +64,13 @@ obsWS.on('ConnectionOpened', () => {
   })
 });
 
-obsWS.on('SceneNameChanged', data => {
-  log('Scene changed to:', data.sceneName);
-});
+obsWS.on('SceneTransitionStarted', data => {
+  log('SceneTransitionStarted', data);
+})
+
+// obsWS.on('SceneNameChanged', data => {
+//   log('Scene changed to:', data.sceneName);
+// });
 
 obsWS.on('CurrentProgramSceneChanged', data => {
   // console.log('Scene changed to:', data.sceneName);
@@ -76,6 +88,15 @@ obsWS.on('CurrentProgramSceneChanged', data => {
 ///////////////////////////////////////////////////////////////
 // Page elements and events
 
+const listOfOBSScenes = [ // not used anywhere, just for reference
+  'scene-camera1',
+  'scene-camera1-pip-media',
+  'scene-media',
+  'scene-media-pip-camera1',
+  'scene-camera2',
+  'scene-camera1-keyed',
+  'scene-camera1-keyed-pip-media'
+];
 
 function sceneButtonsInit(allScenes, currentScene) {
   allScenes.forEach( item => {
