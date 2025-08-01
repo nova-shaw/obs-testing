48e6651b24f511f4344447885eec34f21da9809a
nova-shaw <197215180+nova-shaw@users.noreply.github.com>
2025-08-01 21:12:55 +0900

    Testing setting chroma key color from controlpanel

--- controlpanel.js
+++ controlpanel.js
@@ -1,7 +1,8 @@
 const log = console.log;
 
-const camera1Name = 'src-camera1-ishiguro';
-const camera1KeyFilterName = 'Colour Key';
+const camera1Name = 'Camera1';
+// const camera1KeyFilterName = 'Colour Key';
+const camera1KeyFilterName = 'Chroma Key';
 let keyingEnabled = false;
 
 const obsBC = new BroadcastChannel('obsBC');
@@ -49,14 +50,27 @@ const connection = await obsWS.connect('ws://127.0.0.1:4455', 'huwspZ8H7vTp0CYL'
         sourceName: camera1Name,
         filterName: camera1KeyFilterName
         }).then(data => {
-          // log('GetSourceFilter', data);
+          log('GetSourceFilter', data);
+
+
           keyingEnabled = data.filterEnabled;
           document.body.classList.toggle('keyingOn', keyingEnabled);
-          // log('green screen enabled', keyingEnabled);
+          log('green screen enabled', keyingEnabled);
+
+          logKeyColor(data.filterSettings);
+          const selectColor = document.querySelector(('#keying-color'));
+
+          // selectColor.value = '#00ff00';
+          selectColor.value = `#${andrToHex(data.filterSettings.key_color)}`;
+
+          selectColor.addEventListener('input', e => {
+            log(e.currentTarget.value);
+          })
         });
       obsWS.call('GetHotkeyList').then(data => {
         log('GetHotkeyList', data);
       });
+
     },
     (error) => {
       console.error('OBS not running?', error)
@@ -64,9 +78,63 @@ const connection = await obsWS.connect('ws://127.0.0.1:4455', 'huwspZ8H7vTp0CYL'
     });
 
 
+/**
+ * #00FF00
+ * =
+ * 4278255360
+ * Apparently
+ * AARRGGBB (Alpha-Red-Green-Blue)
+ * `android.graphics.Colors` are in hexadecimal AARRGGBB (Alpha-Red-Green-Blue) format, converted to decimals.
+ */
+
+function logKeyColor(data) {
+  const rgba = androidToRgba(data.key_color);
+  log(data.key_color, rgba);
+  const androidColor = rgbaToAndroid(rgba);
+  log(androidColor);
+
+  const a = andrToHex(data.key_color);
+  log(a);
+  const b = rgbToAndr(a);
+  log(b);
+
+/*
+  const andrOrig = data.key_color;
+  const decToHex = andrOrig.toString(16);
+  const hexTrimmed = decToHex.slice(-6);
+  log(andrOrig, decToHex, hexTrimmed);
+*/
+
+  // Decimal to hexadecimal
+  // hexString = yourNumber.toString(16);
+  // Hexadecimal to decimal
+  // yourNumber = parseInt(hexString, 16);
+}
+
+function andrToHex(color) { // convert decimal to hex, remove first 2 characters (alpha in Anrdoid colors)
+  return color.toString(16).slice(-6);
+}
+
+function rgbToAndr(color) { // add 100% opacity to front, convert hex to decimal
+  return parseInt(`ff${color}`, 16);
+}
 
 
+function androidToRgba(color){
+  const colorArray = []
+  for(let i=0; i<4; i++){
+    colorArray.push(color % 256)
+    color>>>=8
+  }
+  const alpha = colorArray.pop() / 255
+  return `rgba(${colorArray.reverse()},${alpha})`
+}
 
+function rgbaToAndroid(color) {
+  // 'rgba(0,255,0,1)'
+  const str = color.replace(/^rgba?\(|\s+|\)$/g, '');
+  return str;
+}
 
 
 obsWS.on('ConnectionOpened', () => {
@@ -78,8 +146,15 @@ obsWS.on('ConnectionOpened', () => {
 });
 
 
+obsWS.on('SourceFilterSettingsChanged', data => {
+  log('SourceFilterSettingsChanged', data);
+});
+
+
+// SetSourceFilterSettings
+
 obsWS.on('SourceFilterEnableStateChanged', data => {
-  // log('SourceFilterEnableStateChanged', data);
+  log('SourceFilterEnableStateChanged', data);
   if (data.sourceName === camera1Name && data.filterName === camera1KeyFilterName) {
     keyingEnabled = data.filterEnabled;
     document.body.classList.toggle('keyingOn', keyingEnabled);
