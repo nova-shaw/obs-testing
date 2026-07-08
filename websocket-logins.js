94987cc1abac12ede2536919a3710c065291d416
Shaw <2610597+shawneither@users.noreply.github.com>
2026-07-08 12:51:57 +0900

    Adjusting colors, changed websockets login for home work

--- websocket-logins.js
+++ websocket-logins.js
@@ -4,7 +4,7 @@ export const env = {
   // url: '192.168.3.188',
   // url: '192.168.16.94',
   // password: 'BORaMYfZXcq9yFpJ',
-  
-  url: '192.168.50.17',
+
+  url: '192.168.50.8',
   password: '88GIDuYxFLAroLQw',
 }
